1ecbd9a0940fe3e61d45dd9a09037ffe7c3ad13d
Garrett Biernat <garrett@gdbmedia.net>
2016-04-08 16:06:37 -0700

    add more design and almost all of my logic with methods and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,98 @@
+function OrderInfo(name, street, city, state, zip){
+	this.name = name; 
+	this.street = street;
+	this.city = city;  
+	this.state = state;
+	this.zip = zip; 
+}
+function Pizza(size, arrayOfTop) {
+	this.size = size
+	this.array = arrayOfTop;
+}
+Pizza.prototype.price = function(){
+	var price;
+	if(this.size === 1){
+		price = 8.99;
+	}
+	else if(this.size === 2){
+		price = 11.99;
+	}
+	else if(this.size === 3){
+		price = 14.99;
+	}
+	else if(this.size === 4){
+		price = 18.99;
+	}
+	if(this.array.length > 3 ){
+		price += ((this.array.length) - 3) * 0.85; 
+	}
+	
+	return price;
+}
+Pizza.prototype.name = function(){
+	var name
+	if(this.size === 1){
+		name = "Midget";
+	}
+	else if(this.size === 2){
+		name = "Petite Pizza";
+	}
+	else if(this.size === 3){
+		name = "Average Joe";
+	}
+	else if(this.size === 4){
+		name = "Behemoth";
+	}
+	return name;
+}
 $(function(e){
+	var arrayOfPizza = [];
+	var grandTotal = 0;
+	$("#getdeliver").click(function(e){
+		
+		$("#getdeliver").hide();
+		$("#order").hide();
+		$("#create").fadeIn("slow");
+		$("#orderform").fadeIn("slow");
+		
+
+	});
+	$("#orderform").submit(function(e){
+		var topArray = [];
+		
+		var sizeInput = parseInt($("#size").val());
+		console.log(sizeInput);
+		$('input[name="toppings"]:checked').each(function() {
+		   topArray.push(this.value);
+		});
+		
+		arrayOfPizza.push(new Pizza(sizeInput, topArray));
+		var arrayIndex = (arrayOfPizza.length - 1)
+		var pizzaPrice = arrayOfPizza[arrayIndex].price();
+		var pizzaName = arrayOfPizza[arrayIndex].name();
+		var topnum = arrayOfPizza[arrayIndex].array.length;
+		grandTotal += pizzaPrice;
+		$("#create").hide();
+		$("#receipt").fadeIn("slow");
+		$("#orderform").hide();
+		$("#totalDisplay").fadeIn("slow");
+		$("#display").append("<div class='pizza'><h1>1 " + pizzaName + " with " + topnum + " Toppings</h1><h2>Price: $" + pizzaPrice.toFixed(2) + "</h2><br><br><br></div><div><hr></div>")
+		// $("#pizzaname1").text(pizzaName);
+		// $("#total1").text("Price: $" + pizzaPrice);
+		$(".total").fadeIn("slow");
+		$("#total").text("$" + (grandTotal + 2.99).toFixed(2));
+
+		$("#orderform")[0].reset();
+		return false;
+	});
+	$("#place").click(function(){
+		$("#receipt").hide();
+		$("#placeorder").fadeIn("slow");
+		$("#totalDisplay").hide();
+		$("#infoform").fadeIn("slow");
+		
+
+	});
 	$("#infoform").submit(function(e){
 		var nameInput = $("#name").val();
 		var streetInput = $("#street").val();
@@ -6,10 +100,33 @@ $(function(e){
 		var stateInput = $("#state").val();
 		var zipInput = $("#zip").val();
 
+		var newOrderInfo = new OrderInfo(nameInput, streetInput, cityInput, stateInput, zipInput);
+		console.log(newOrderInfo.name);
+		console.log(newOrderInfo.street);
+		console.log(newOrderInfo.city);
+		console.log(newOrderInfo.state);
+
 		$("#infoform").hide();
-		$("#order").hide();
-		$("#create").fadeIn("slow");
+		$("#placeorder").hide();
+		$("#orderplaced").fadeIn("slow");
+		$("#deliverymessage").fadeIn("slow");
+		$("#namedisplay").text(newOrderInfo.name);
+		$("#streetdisplay").text(newOrderInfo.street);
+		$("#addressdisplay").text(newOrderInfo.city + ", " + newOrderInfo.state + " " + newOrderInfo.zip);
 		$("#infoform")[0].reset();
 		return false;
+	});
+	$("#refresh").click(function(){
+		location.reload();
+		
+
+	});
+	$("#add").click(function(){
+		$("#totalDisplay").hide();
+		$("#receipt").hide();
+		$("#create").fadeIn("slow");
+		$("#orderform").fadeIn("slow");
+		
+
 	});
 });
